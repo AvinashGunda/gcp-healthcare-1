0e9474557bdff736f612ab3cedbbf911d913263a
AvinashGunda <avinash.iitk11@gmail.com>
2025-12-04 19:04:14 +0530

    Initial commit - added pipeline code

---
+++ data/configs/audit_table_ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE avigunda1234.temp_dataset.audit_log (
+    data_source STRING,
+    tablename STRING,
+    load_type STRING,
+    record_count INT64,
+    load_timestamp TIMESTAMP,
+    status STRING
+);
